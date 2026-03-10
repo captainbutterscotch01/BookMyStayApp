675d7121540ca6f24d5dbd5e208f19db294f8750
abhinav <labhinav06@gmail.com>
2026-03-10 15:10:02 +0530

    Use Case 1 - Application Entry and Welcome Message

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+/**
+ * Abstract class representing a general Room.
+ * It defines common properties shared by all room types.
+ */
+
+abstract class Room {
+
+    protected int beds;
+    protected int size;
+    protected double price;
+
+    public Room(int beds, int size, double price) {
+        this.beds = beds;
+        this.size = size;
+        this.price = price;
+    }
+
+    public void displayDetails() {
+        System.out.println("Beds: " + beds);
+        System.out.println("Size: " + size + " sq ft");
+        System.out.println("Price: $" + price);
+    }
+}
