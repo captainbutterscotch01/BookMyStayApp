2a00dac81d8e71648681bc26912005ec07066be9
captainbutterscotch01 <labhinav06@gmail.com>
2026-03-17 14:59:33 +0530

    UC1

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class HotelBookingApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Hotel Booking System");
+        System.out.println("Application Name: Hotel Booking System");
+        System.out.println("Version: 1.0");
+        System.out.println("Application started successfully.");
+    }
+}
